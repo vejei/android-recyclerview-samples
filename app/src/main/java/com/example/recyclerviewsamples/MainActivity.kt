313386a2631b5e0a6dcd4279cb8dc6506a628d08
vejei <chengsnem@163.com>
2020-04-27 19:45:24 +0800

    Set action bar title

--- app/src/main/java/com/example/recyclerviewsamples/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewsamples/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.recyclerviewsamples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.recyclerviewsamples.endlessload.EndlessLoadFragment
+import com.example.recyclerviewsamples.endlessload.NestedScrollViewEndlessLoadFragment
+import com.example.recyclerviewsamples.endlessload.RecyclerViewEndlessLoadFragment
 import com.example.recyclerviewsamples.grid.RecyclerViewFragment
 import com.example.recyclerviewsamples.grid.GridFragment
 import com.example.recyclerviewsamples.grid.GridViewFragment
 import com.example.recyclerviewsamples.itemdecoration.DividerFragment
 import com.example.recyclerviewsamples.itemdecoration.GapFragment
 import com.example.recyclerviewsamples.itemdecoration.ItemDecorationFragment
+import com.example.recyclerviewsamples.staggeredgrid.StaggeredGridFragment
+import com.example.recyclerviewsamples.staggeredgrid.StaggeredGridLayoutManagerFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
                 is MainFragment -> R.string.app_name
                 is ItemDecorationFragment, is DividerFragment, is GapFragment -> R.string.main_item_decoration_button_text
                 is GridFragment, is RecyclerViewFragment, is GridViewFragment -> R.string.main_grid_list_button_text
+                is StaggeredGridFragment, is StaggeredGridLayoutManagerFragment -> R.string.main_staggered_grid_button_text
+                is EndlessLoadFragment, is RecyclerViewEndlessLoadFragment, is NestedScrollViewEndlessLoadFragment -> R.string.main_endless_load_button_text
                 else -> R.string.app_name
             }
             supportActionBar?.setTitle(titleRes)
